abbcd7ec53f93bf7777997197f1b0a448ac63e9e
Adrien KISSIE <fredkiss3@gmail.com>
2023-03-27 21:00:17 +0200

    cache json everywhere

--- app/routes/ssr-blocking.tsx
+++ app/routes/ssr-blocking.tsx
@@ -1,10 +1,18 @@
 import { wait } from "~/lib.server";
+import { json } from "@vercel/remix";
 
 export async function loader() {
   await wait(100);
-  return {
-    streamed: "ok",
-  };
+  return json(
+    {
+      blocking: "ok",
+    },
+    {
+      headers: {
+        "cache-control": "public, s-max-age=86400, stale-while-revalidate",
+      },
+    }
+  );
 }
 export default function SSRBlocking() {
   return (
